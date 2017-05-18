4c0815e0119460e74850bf244a18f43bd720f483
haixia <hxliu@nltechdev.com>
2017-05-18 15:34:04 +0800

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-project1InPods_Example/Pods-project1InPods_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_project1InPods_Example : NSObject
+@end
+@implementation PodsDummy_Pods_project1InPods_Example
+@end
---
+++ Example/Pods/Target Support Files/project1InPods/project1InPods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_project1InPods : NSObject
+@end
+@implementation PodsDummy_project1InPods
+@end
